a17c3a82f8365e0e05e1a4adf1b882ec77f7d39a
rhymes <rhymes@ff783c49-aaf6-0310-8f5a-cb3293e28163>
2005-05-18 23:35:54 +0000

    improved testing

--- trunk/test/functional/paste_controller_test.rb
+++ trunk/test/functional/paste_controller_test.rb
@@ -23,6 +23,8 @@ class PasteControllerTest < Test::Unit::TestCase
     assert_response :success
     assert_not_nil assigns(:pastes)
     assert_not_nil assigns(:paste)
+    assert_equal 1, assigns(:paste)[:id]
+    assert flash.empty?
     assert_template "show"
   end
 
@@ -32,24 +34,31 @@ class PasteControllerTest < Test::Unit::TestCase
     assert_not_nil assigns(:paste)
   end
 
-#  def test_create
-#    post :create, { 
-#      :paste => { 
-#        "author" => "anonymous",
-#        "language" => "Ruby",
-#        "description" => "test",
-#        "body" => "def foo end",
-#        "created_on" => Time.now
-#      }
-#    }
+  def test_create
+    post :create, { 
+      "author" => "anonymous",
+      "language" => "Ruby",
+      "description" => "test",
+      "body" => "def foo end",
+      "created_on" => Time.now
+    }
     
-#    assert_equal "anonymous", cookies[$AUTHOR_COOKIE_NAME][0]
+    assert_equal "anonymous", cookies[$AUTHOR_COOKIE_NAME][0]
+    assert_response :redirect
 
-#    assert_response :redirect
+    paste = Paste.find(:all, :order => "id")[-1]
+    assert paste.save
 
-#    paste = Paste.find(:all, :order => "id")[-1]
-#    assert paste.save
+    assert_redirected_to :action => "show"#, :id => paste.id
+  end
 
-#    assert_redirected_to :action => "show"#, :id => paste.id
-#  end
+  def test_show_routing
+    opts = { :controller => "paste", :action => "show", :id => "1" }
+    assert_routing "paste/show/1", opts
+  end
+
+  def test_download_routing
+    opts = { :controller => "paste", :action => "download", :id => "1" }
+    assert_routing "paste/download/1", opts
+  end
 end
