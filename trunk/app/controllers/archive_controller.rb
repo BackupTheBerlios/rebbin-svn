1de28639084d07019c8bc8265a8ead7eaba71a77
rhymes <rhymes@ff783c49-aaf6-0310-8f5a-cb3293e28163>
2005-05-19 22:50:17 +0000

    Improved sorting in archive page

--- trunk/app/controllers/archive_controller.rb
+++ trunk/app/controllers/archive_controller.rb
@@ -1,25 +1,10 @@
 class ArchiveController < ApplicationController
+  helper :sort
+  include SortHelper
+
   def index
-    @pastes = Paste.find_all
-  end
-
-  def sort_by_author
-    @pastes = Paste.find_all_by_author
-    render_action "index"
-  end
-  
-  def sort_by_language
-    @pastes = Paste.find_all_by_language
-    render_action "index"
-  end
-  
-  def sort_by_description
-    @pastes = Paste.find_all_by_description
-    render_action "index"
-  end
-
-  def sort_by_date
-    @pastes = Paste.find_all_by_date
-    render_action "index"
+    sort_init "created_on", "desc"
+    sort_update
+    @pastes = Paste.find(:all, :order => sort_clause)
   end
 end
