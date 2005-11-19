03574350bba7b117695859a62c106f69af371971
rhymes <rhymes@ff783c49-aaf6-0310-8f5a-cb3293e28163>
2005-11-19 22:53:49 +0000

    add comments table to mysql schema

--- trunk/db/mysql_schema.sql
+++ trunk/db/mysql_schema.sql
@@ -14,3 +14,14 @@ create table `pastes`
     `body` text not null,
     `created_on` timestamp not null
 ) TYPE=InnoDB CHARSET='utf8';
+
+create table `comments`
+(
+    `id` int unsigned auto_increment primary key,
+    `paste_id` int unsigned references pastes (id) on update cascade on delete cascade,
+    `author` varchar(30) not null,
+    `email` varchar(40),
+    `uri` varchar(70),
+    `body` text not null,
+    `created_on` timestamp not null
+) TYPE=InnoDB CHARSET='utf8';
