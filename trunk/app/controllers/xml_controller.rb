b6ba72b6bd1b6c53213498153d902c5a94e78762
rhymes <rhymes@ff783c49-aaf6-0310-8f5a-cb3293e28163>
2006-03-17 17:38:29 +0000

    get rid of atom 0.3

--- trunk/app/controllers/xml_controller.rb
+++ trunk/app/controllers/xml_controller.rb
@@ -1,16 +1,12 @@
 class XmlController < ApplicationController
-  caches_page :rss, :atom03, :atom10
+  caches_page :rss, :atom
   session :off
 
   def rss
     @pastes = Paste.find_latest_pastes
   end
 
-  def atom03
-    rss
-  end
-
-  def atom10
+  def atom
     rss
   end
 end
